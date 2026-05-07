18ae14656d825dd85c3190979192cc01165e5911
chun9l <97897047+chun9l@users.noreply.github.com>
2026-05-07 10:40:34 +0100

    Add box plot for idle time

--- plotting/plot_idle.py
+++ plotting/plot_idle.py
@@ -50,17 +50,16 @@ def compute_elapsed(df):
     The time in the raw logs are given in datetime format 
     as in DATETIME_FMT. This converts them to elapsed time in minutes.
     """
-    reference = df["start"].min()
+    reference = df["start"].min() # Find the earliest start time
 
-    df["start_min"] = (df["start"] - reference).dt.total_seconds() / 60
-    df["duration_min"] = (df["end"] - df["start"]).dt.total_seconds() / 60
-
+    df["start_sec"] = (df["start"] - reference).dt.total_seconds()
+    df["end_sec"] = (df["end"] - reference).dt.total_seconds() 
     return df, reference
 
 def plot(
     log_files,
     *,
-    title="Boxplots of idle time",
+    title=None,
     show_legend=True,
     show=False,
     save_path=None,
@@ -71,37 +70,36 @@ def plot(
     node_data, ref_dt = compute_elapsed(node_data)
 
     fig, ax = plt.subplots(constrained_layout=True)
+    fig.suptitle(title)
     
+    idle_time = []
     # Get idle time between requests
-    for y_idx, (node, grp) in enumerate(df.groupby("slurm_id")):
-        starts = grp["start_min"].to_numpy()
-        widths = grp["duration_min"].to_numpy()
+    for y_idx, (node, grp) in enumerate(node_data.groupby("slurm_id")):
+        start = grp["start_sec"]
+        end = grp["end_sec"]
+        
+        delay = start - end.shift(1)
+        idle_time.append(delay.to_numpy()[1:])
 
     # Plot boxplots
-    hq = ax.boxplot(data_hq, positions=np.array(range(len(data_hq))) * 2.0 + 0.4, meanline=True, showmeans=True,
+    hq = ax.boxplot(idle_time, meanline=True, showmeans=True,
                         meanprops={"linestyle": "--", "color": "black", "linewidth": "1.5"},
                         medianprops={"linestyle": "-", "color": "black", "linewidth": "1.5"})
-    ax.set_xticks(range(0, len(benchmark) * 2, 2), benchmark)
-    plt.plot([], 'b-', linewidth=1, label="SLURM")
-    plt.plot([], 'r-', linewidth=1, label="HQ")
-    plt.plot([], 'k-', linewidth=1.5, label="Median")
-    plt.plot([], 'k--', linewidth=1.5, label="Mean")
-    plt.plot([], 'ko', markerfacecolor='white', label="Fliers")
+
+    ax.plot([], 'k-', linewidth=1.5, label="Median")
+    ax.plot([], 'k--', linewidth=1.5, label="Mean")
+    ax.plot([], 'ko', markerfacecolor='white', label="Fliers")
     ax.set_yscale("log")
 
     node_labels = list(node_data["slurm_id"].unique())
     
-    ax.set_yticks(range(len(node_labels)))
-    ax.set_yticklabels([f"Task {i}" for i in range(len(node_labels))])
+    ax.set_xticks(range(1, len(node_labels) + 1))
+    ax.set_xticklabels([f"Task {i}" for i in range(len(node_labels))])
 
-    ax.set_xlabel(
-        f"Elapsed time (minutes)\n[reference: {ref_dt.strftime(DATETIME_FMT)}]"
+    ax.set_ylabel(
+        f"Idle time [s]"
     )
     
-    plt.plot([], 'k-', linewidth=1.5, label="Median")
-    plt.plot([], 'k--', linewidth=1.5, label="Mean")
-    plt.plot([], 'ko', markerfacecolor='white', label="Fliers")
-
     if show_legend:
         ax.legend(
             loc="upper right",
@@ -116,36 +114,6 @@ def plot(
         plt.show()
 
     return fig
-
-os.chdir("/home/ming/UMBridge_Loadbalancer/")
-for i in range(len(metrics)):
-    for job in job_count:
-        fig, ax = plt.subplots()
-        fig.suptitle(f"{metrics[i]} {job} jobs")
-        if i != 3:
-            ax.set_ylabel("Time (s)")
-        else:
-            ax.set_ylabel("Arbitrary units")
-        data_slurm = [data_dict[scheduler[0].lower()][job][metrics[i]][app] for app in benchmark]
-        data_hq = [data_dict[scheduler[1].lower()][job][metrics[i]][app] for app in benchmark]
-        slurm = ax.boxplot(data_slurm, positions=np.array(range(len(data_slurm))) * 2 - 0.4, meanline=True,
-                           showmeans=True, meanprops={"linestyle": "--", "color": "black", "linewidth": "1.5"},
-                           medianprops={"linestyle": "-", "color": "black", "linewidth": "1.5"})
-        hq = ax.boxplot(data_hq, positions=np.array(range(len(data_hq))) * 2.0 + 0.4, meanline=True, showmeans=True,
-                        meanprops={"linestyle": "--", "color": "black", "linewidth": "1.5"},
-                        medianprops={"linestyle": "-", "color": "black", "linewidth": "1.5"})
-        ax.set_xticks(range(0, len(benchmark) * 2, 2), benchmark)
-        set_fill_color(slurm, "blue")
-        set_fill_color(hq, "red")
-        plt.plot([], 'b-', linewidth=1, label="SLURM")
-        plt.plot([], 'r-', linewidth=1, label="HQ")
-        plt.plot([], 'k-', linewidth=1.5, label="Median")
-        plt.plot([], 'k--', linewidth=1.5, label="Mean")
-        plt.plot([], 'ko', markerfacecolor='white', label="Fliers")
-        if metrics[i] != "SLR":  ax.set_yscale("log")
-        plt.legend()
-        # plt.show()
-        # plt.savefig(f"{metrics[i]}_{job}.pdf", format="pdf")
         
 if __name__ == "__main__":
     slurm_job_id = "10956759"
@@ -153,10 +121,8 @@ if __name__ == "__main__":
     log_files = {f"{slurm_job_id}_{i}": f"{file_path + slurm_job_id}_{i}/active_time.log" for i in range(1, 6)}
     plot(
         log_files,
-        show_dep            = True,
-        arrow_chains        = [2],
-        title               = "Process Uptime",
+        title               = "Process idle time",
         # show                = True,
-        show_legend         = False,
-        save_path           = f"uptime_{slurm_job_id}.png",
+        show_legend         = True,
+        save_path           = f"idle_time_{slurm_job_id}.svg",
     )
